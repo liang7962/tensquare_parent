8579992970f371f0d0779b67dec849a621fa16c6
liang7962 <liang4032>
2019-09-18 18:33:16 +0800

    代码提交

---
+++ tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -0,0 +1,20 @@
+package com.tensquare.qa.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.tensquare.qa.pojo.Problem;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * 数据访问接口
+ * @author Administrator
+ *
+ */
+public interface ProblemDao extends JpaRepository<Problem,String>,JpaSpecificationExecutor<Problem>{
+
+    @Query("select p from Problem p where id in( select problemid from Pl where labelid=? ) order by replytime desc")
+    public Page<Problem> findNewListByLabelId(String labelId, Pageable pageable);
+}
